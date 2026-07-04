9a04c1477152f537cc6fb0c72e412a227fa050f7
Sihao Liu <liu@sihao.me>
2026-07-04 08:39:25 +0000

    Revert "Inline row step cursor helpers"
    This reverts commit b5ad7515febc9cf7a983a35582283e1a7641915b.

--- crates/lzvm-prover/src/guest_pc_trace_backend.rs
+++ crates/lzvm-prover/src/guest_pc_trace_backend.rs
@@ -10780,7 +10780,6 @@ impl GuestPcTraceRowMemStepCursor {
         })
     }
 
-    #[inline(always)]
     fn advance_to(&mut self, row: usize) -> Result<(), GuestPcTraceBackendError> {
         if row == self.current_row {
             return Ok(());
@@ -10807,7 +10806,6 @@ impl GuestPcTraceRowMemStepCursor {
         Ok(())
     }
 
-    #[inline(always)]
     fn base(&self) -> u64 {
         self.current_base
     }
@@ -10853,7 +10851,6 @@ impl<'a> ZiskMainReportValidationContext<'a> {
         })
     }
 
-    #[inline(always)]
     fn row_mem_step_base(&mut self, row: usize) -> Result<u64, GuestPcTraceBackendError> {
         self.row_mem_step_cursor.advance_to(row)?;
         Ok(self.row_mem_step_cursor.base())
